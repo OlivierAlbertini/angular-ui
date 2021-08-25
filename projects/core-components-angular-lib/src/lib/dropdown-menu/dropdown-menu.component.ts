5a53c93704022f43525e4b3f5dfa269b2b2eedcc
ouhoc <ouafa.hocine@montreal.ca>
2021-08-25 14:25:34 -0400

    feat: Add check box

--- projects/core-components-angular-lib/src/lib/dropdown-menu/dropdown-menu.component.ts
+++ projects/core-components-angular-lib/src/lib/dropdown-menu/dropdown-menu.component.ts
@@ -1,4 +1,4 @@
-import {  Component, Directive, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Directive, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'bao-dropdown-menu',
@@ -7,12 +7,9 @@ import {  Component, Directive, Input, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class BaoDropDownMenuComponent {
-
   @Input() public isOpen = false;
   @Input() public triggerOrigin: any;
-  @Input() public width: string = '256px';
 }
-
 @Component({
   selector: 'bao-divider, [bao-divider], [baoDivider]',
   templateUrl: 'divider.html',
@@ -25,15 +22,11 @@ export class BaoDividerComponent {}
  */
 @Directive({
   selector: 'bao-dropdown-menu-item, [bao-dropdown-menu-item], [baoDropdownMenuItem]',
-  host: { class: 'bao-dropdown-menu-item',
-  "[class.bao-dropdown-menu-item-disabled]":"isDisabled===true"
-}
+  host: { class: 'bao-dropdown-menu-item', '[class.bao-dropdown-menu-item-disabled]': 'isDisabled===true' }
 })
 export class BaoDropDownMenuItemComponent {
   @Input() public isDisabled = false;
 }
-
-
 @Directive({
   selector: 'bao-dropdown-menu-title, [bao-dropdown-menu-title], [baoDropdownMenuTitle]',
   host: { class: 'bao-dropdown-menu-title' }
@@ -47,12 +40,10 @@ export class BaoDropDownMenuTitleComponent {}
 export class BaoDropDownMenuCheckboxComponent {}
 @Directive({
   selector: 'bao-dropdown-menu-label, [bao-dropdown-menu-label], [baoDropdownMenuLabel]',
-  host: { class: 'bao-dropdown-menu-label',
-  "[class.bao-dropdown-menu-item-disabled]":"isDisabled===true" }
+  host: { class: 'bao-dropdown-menu-label', '[class.bao-dropdown-menu-item-disabled]': 'isDisabled===true' }
 })
 export class BaoDropDownMenuLabelComponent {
   @Input() public isDisabled = false;
-
 }
 @Directive({
   selector: 'bao-dropdown-menu-description, [bao-dropdown-menu-description], [baoDropdownMenuDescription]',
